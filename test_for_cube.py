147ceca81f003b4b4be2a09f95c75039bf0e96be
Pooja Yadav <ypooja510@gmail.com>
2016-05-05 15:25:49 +0530

    add test to check cubes for different numbers

--- test_for_cube.py
+++ test_for_cube.py
@@ -7,5 +7,8 @@ import unittest
 class Mytest(unittest.TestCase):
     def test_cube(self):
         self.assertEqual(cube(3),27)
-
+        self.assertEqual(cube(2),8)
+        self.assertEqual(cube(-1),-1)
+        self.assertEqual(cube(0),0)
 unittest.main()
+
