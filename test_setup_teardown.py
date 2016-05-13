b4498556ae216d760d57c0af121637852fa79cbe
Pooja Yadav <ypooja510@gmail.com>
2016-05-13 09:29:20 +0530

    add programme for setup and teardown

---
+++ test_setup_teardown.py
@@ -0,0 +1,17 @@
+import unittest
+
+class Testfixtures(unittest.TestCase):
+    def setUp(self):
+        print(" In setUp()")
+        self.fixture = range(1,6)
+
+    def test(self):
+        self.assertEqual(self.fixture,range(1,6),"test failed")
+
+    def tearDown(self):
+        print("In TearDown()")
+        del self.fixture
+
+
+if __name__ == "__main__":
+    unittest.main()
