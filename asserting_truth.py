168c87b68da2a7c77bdb10216b026f6230d06520
Pooja Yadav <ypooja510@gmail.com>
2016-05-13 09:16:29 +0530

    add asserting truth test

---
+++ asserting_truth.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Assertion_truth(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+
+    def test_false(self):
+        self.assertFalse(True,"test failed")
+
+if __name__ == "__main__":
+    unittest.main()
